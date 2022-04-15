f5eba423b0aa03d1790b26e4f83354c9b44df71c
Jhai Navarro <jnavarro@tauria.com>
2022-04-16 01:54:35 +0800

    Login form UI

--- src/app/auth/login/Login.styles.ts
+++ src/app/auth/login/Login.styles.ts
@@ -1,5 +1,48 @@
 import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
-  //
+  form: {
+    height: "100%",
+    width: "100%",
+    paddingInline: theme.spacing.xl * 4,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+
+  title: {
+    marginBottom: theme.spacing.lg,
+  },
+
+  socials: {
+    display: "flex",
+    justifyContent: "center",
+    columnGap: theme.spacing.sm,
+    marginBottom: theme.spacing.md,
+
+    "> button": {
+      border: "none",
+      borderRadius: "50%",
+      boxShadow: "0px 4px 8px -2px rgb(0 0 0 / 50%)",
+    },
+  },
+
+  input: {
+    ":not(:first-of-type)": {
+      marginTop: theme.spacing.sm,
+    },
+  },
+
+  forgotPw: {
+    fontSize: theme.fontSizes.xs,
+    textAlign: "left",
+    marginTop: theme.spacing.sm,
+    color: theme.colors.pink[5],
+  },
+
+  submit: {
+    marginTop: theme.spacing.xl,
+  },
 }));
--- src/app/auth/login/Login.tsx
+++ src/app/auth/login/Login.tsx
@@ -1,4 +1,5 @@
 import {
+  ActionIcon,
   Anchor,
   Button,
   PasswordInput,
@@ -6,26 +7,47 @@ import {
   TextInput,
   Title,
 } from "@mantine/core";
+import { ReactComponent as Google } from "shared/assets/google.svg";
+import { ReactComponent as Twitter } from "shared/assets/twitter.svg";
+import { ReactComponent as Facebook } from "shared/assets/facebook.svg";
+import { useStyles } from "./Login.styles";
+import { Link } from "react-router-dom";
 
-export function Login() {
+interface LoginProps {
+  className?: string;
+}
+
+export function Login({ className }: LoginProps) {
+  const { classes } = useStyles();
+
   return (
-    <div>
-      <form>
-        <Title>Sign in</Title>
+    <div className={className}>
+      <form className={classes.form}>
+        <Title className={classes.title}>Sign in</Title>
 
-        <div>
-          <Button>Log in with Google</Button>
-          <Button>Log in with Twitter</Button>
-          <Button>Log in with Facebook</Button>
+        <div className={classes.socials}>
+          <ActionIcon size="lg">
+            <Google />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <Twitter />
+          </ActionIcon>
+          <ActionIcon size="lg">
+            <Facebook />
+          </ActionIcon>
         </div>
 
-        <Text>or use your account</Text>
+        <Text color="gray">——— or ———</Text>
 
-        <TextInput placeholder="Email" />
-        <PasswordInput placeholder="Password" />
-        <Anchor href=".">Forgot your password?</Anchor>
+        <TextInput className={classes.input} placeholder="Email" />
+        <PasswordInput className={classes.input} placeholder="Password" />
 
-        <Button>Sign In</Button>
+        {/* TODO: Forgot pw flow */}
+        <Anchor className={classes.forgotPw} component={Link} to="/">
+          Forgot your password?
+        </Anchor>
+
+        <Button className={classes.submit}>Sign In</Button>
       </form>
     </div>
   );
